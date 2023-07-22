f5021f695deaf92fff2cdce39aed83d6ffe6571b
Sergey <iakobian@gmail.com>
2023-07-22 20:36:48 +0600

    Less memory for potential reading

--- read_forcefield.h
+++ read_forcefield.h
@@ -7,67 +7,68 @@ void read_forcefield (const char * filename, vector <vector <vector <double> > >
 		assert (pf != 0);
 		assert (ferror(pf) == 0);
 
-    vector<double> distance;
-    vector<double> angle_1;
-    vector<double> angle_2;
-    vector<double> energy;
-
 	  double line[4];
+		double first_dist = -1;
+		double first_a1;
+		double first_a2;
+		double now_dist = -100;
+		double now_a1 = -100;
+		double now_a2 = -100;
+		int i = 0;
+		int j = 0;
+		int k = -1;
+		int counter_dist = 0;
+		int counter_da = 0;
+		int counter_da2 = 0;
 		while( 4 == fscanf (pf, "%lf %lf %lf %lf", line, line+1, line+2, line+3 ))
     {
 				assert (ferror(pf) == 0);
 				assert (feof(pf) == 0);
-        distance.push_back(line[0]);
-        angle_1.push_back(line[1]);
-        angle_2.push_back(line[2]);
-        energy.push_back(line[3]);
+				if (first_dist == -1)
+				{
+					first_dist = line[0];
+					first_a1 = line[1];
+					first_a2 = line[2];
+
+					now_a1 = line[1];
+					now_dist = line[0];
+				}
+
+				k++;
+				if (now_a1 != line[1])
+				{
+					da = line[1] - now_a1;
+					k = 0;
+					j++;
+					if (now_dist != line[0])
+					{
+						cout << "Now r:" << line[0] << endl;
+						dr = line[0] - now_dist;
+						j = 0;
+						i++;
+					}
+					now_dist = line[0];
+				}
+				now_a1 = line[1];
+
+				forcefield[i][j][k] = line[3] * 4184.0;
+
     }
 		assert (feof(pf) != 0);
 
     //Close the file stream
 		fclose(pf);
 
-    double steps = distance.size();
-    double angle_step_2;
+		min_dist = first_dist;
+		max_dist = now_dist;
 
-    min_dist = distance[0];
-		max_dist = distance[steps-1];
+		cout << "min:" << min_dist << " max:" << max_dist << endl;
+		cout << "dr:" << dr << " da:" << da << endl;
+		cout << "max_ijk:" << i << " " << j << " " << k << endl;
 
-    for (int i=1;i<steps;i++)
-    {
-        if ((distance[i]-distance[0]) > 1e-5)
-        {
-           dr = distance[i]-distance[0];
-           break;
-        }
-    }
-    for (int i=1;i<steps;i++)
-    {
-        if ((angle_1[i]-angle_1[0]) > 1e-5)
-        {
-           da = angle_1[i]-angle_1[0];
-           break;
-        }
-    }
-    for (int i=1;i<steps;i++)
-    {
-        if ((angle_2[i]-angle_2[0]) > 1e-5)
-        {
-           angle_step_2 = angle_2[i]-angle_2[0];
-           break;
-        }
-    }
-
-    long int dist,ang1,ang2;
-    for (long int i = 0; i < steps; i++) {
-        dist = (int)(((distance[i]-distance[0])/dr)+0.5);
-        ang1 = (int)(((angle_1[i]-angle_1[0])/da)+0.5);
-        ang2 = (int)(((angle_2[i]-angle_2[0])/angle_step_2)+0.5);
-        // 1 kcal = 4184 J/mol
-        forcefield[dist][ang1][ang2] = energy[i]*4184.0;
-        //forcefield[dist][ang1][ang2] = energy[i];
-    }
-
+			int dist = i;
+			int ang1 = j;
+			int ang2 = k;
 			int a1, a2;
 			for (int i = 0; i <= dist; i++)
 			{
@@ -77,13 +78,13 @@ void read_forcefield (const char * filename, vector <vector <vector <double> > >
 					if (a1 >= int(360 / da + 0.5)) {a1 -= int(360 / da + 0.5);}
 					for(int k = 0; k <= ang2; k++)
 					{
-						a2 = k + int(180 / angle_step_2 + 0.5);
-						if (a2 >= int(360 / angle_step_2 + 0.5)) {a2 -= int(360 / angle_step_2 + 0.5);}
+						a2 = k + int(180 / da + 0.5);
+						if (a2 >= int(360 / da + 0.5)) {a2 -= int(360 / da + 0.5);}
 						// Test of the potential for symmetry
-//						if (abs(forcefield[i][j][k]-forcefield[i][a2][a1]) > 0.15*abs(forcefield[i][a2][a1]))
-						if (abs(forcefield[i][j][k]-forcefield[i][a2][a1]) > 1000 && abs(forcefield[i][j][k]) < E_INF*R*1000.0)
+						//if (abs(forcefield[i][j][k]-forcefield[i][a2][a1]) > 0.15*abs(forcefield[i][a2][a1]))
+						if (abs(forcefield[i][j][k]-forcefield[i][a2][a1]) > 1000 && abs(forcefield[i][j][k]) < E_INF * R * 1000.0)
 								{
-									cout << "distance: " << min_dist + i * dr << " angle1: " << j << " angle2: " << k << endl;
+									cout << "distance: " << min_dist + i * dr << " angle1: " << j * da << " angle2: " << k * da << endl;
 									cout << "Potential error. Energy=" << forcefield[i][j][k] << " must be close energy=" << forcefield[i][a2][a1] << endl;
 								}
 						if (forcefield[i][j][k] < forcefield[i][a2][a1])
@@ -97,32 +98,4 @@ void read_forcefield (const char * filename, vector <vector <vector <double> > >
 					}
 				}
 			}
-/*
-		// Testing for anges in the simple potential
-		for (int i = 0; i <= dist; i++)
-		{
-			for (int j = 0; j <= ang1; j++)
-			{
-				for(int k = 0; k <= ang2; k++)
-				{
-					if (forcefield[i][j][k] < -40000.0)
-					{
-						int aa_plus = j + 1;
-						int aa_minus = j - 1;
-						if (aa_plus == ang1)
-						{
-							aa_plus = 0;
-						}
-						if (aa_minus == -1)
-						{
-							aa_minus = ang1 - 1;
-						}
-						cout << "before:" << forcefield[i][j][k] << endl;
-						forcefield[i][j][k] = (forcefield[i][aa_plus][k] + forcefield[i][aa_minus][k])/2.0;
-						cout << "after:" << forcefield[i][j][k] << endl;
-					}
-				}
-			}
-		}*/
-
 }
