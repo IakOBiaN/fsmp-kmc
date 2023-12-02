bfd0ea87197014b5cb0bd64c803554cc007aa3f5
iakobian <iakobian@gmail.com>
2023-12-02 20:08:14 +0600

    New files structure

--- program_body.cpp
+++ program_body.cpp
@@ -1,18 +1,3 @@
-#include <vector>
-#include <cmath>
-#include <iostream>
-#include <cstdlib>
-#include <math.h>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <iomanip>
-#include "random/sfmt.h"
-#include "random/sfmt.cpp"
-#include <cmath>
-#include <float.h>
-using namespace std;
-
 // Random number generator
 int seed = (int)time(0);
 CRandomSFMT0 RanGen(seed);
@@ -58,24 +43,7 @@ results::results(void) {
    p_X = 0;
    p_Y = 0;
 }
-/*
-// Class contains the function descrabing the state of the molecule:
-// 1) coordinates
-// 2) angles
-// 3) charge distribution
-class state {
-public:
-double x;
-double y;
-double phi;
-double sin_phi;
-double cos_phi;
-double damping_coeff;
-results ex_field_coeff;
-double stat_weight;
-results en_and_pr;
-};
-*/
+
 class state {
 public:
 double x;
@@ -95,82 +63,6 @@ double R = 8.31446261815324;										// Gas constant in J per mol
 double N_a = 6.02214076e+23;										//	Avogadro constant
 double E_INF = 75.0;											// in kT units
 const double PI = 3.14159265358979323846;
-
-///////////////////////////////////////////////////////////////////////////////
-//////////////// CONFIGURATION ////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-
-//MAIN PARAMETERS
-//temperature in crystal
-double temp_from = 300;
-double temp_to = 301;
-double temp_step = 10;
-//parameter of the external field
-double um_from = 0.0;
-double um_to = -100000.0;
-double um_step = -1000.0;
-/////////// Set the lenght of MC run ///////////////////////////////
-int nSteps = 1000000;            // Total amount of MCS
-int nStepsEq = 500000;           // MCS for relaxation
-bool constant_pressure = false;
-double constant_pressure_value = 0;
-bool kMC = true;
-
-//MODEL SETTINGS
-//you can use your own structures if set "structure_name" to "calculate"
-string structure_name = "TPA_chain_qPBE_Dreiding_Dhb5.0";
-int uc_in_x = 14;
-int uc_in_y = 6;
-double free_space = 0.24;
-/////// molecules visualization block (2 or 3 directions) /////////////
-int total_molecule_directions = 2;
-//for TPA
-double angle_1 = 180;
-double angle_2 = 240;       // it can be any angle as long as total_molecule_directions is 2
-
-//ADDITIONAL PARAMETERS
-double delta = 2.0;													// MC parameter. Maximal shift of the molecule
-double delta_angle = 60.0;    										// MC parameter. Maximal rotation in degrees
-double temperature_in_transition_zone = 800;						// K
-double lambdam = 0.0;
-bool numeric = true;
-bool widom_test_index = false;
-
-//FILE NAMES
-//numerical potential name
-string temp_name = "TPA_qPBE_crystal_Dhb5.0_r5.5_16_dr01_da5.dat";
-const char * potential_name = temp_name.c_str();
-//name of xyz file of unit cell optimization (if you want to optimize a unit cell)
-string unit_cell_name = "0_calculate_animation.xyz";
-//name of xyz file for visualization
-string xyz_name = "1_xyz_for_calculations_TPA_hor.xyz";
-stringstream name_of_file_for_statistics;
-void complex_names()
-{
-  name_of_file_for_statistics << "2_statistics_" << structure_name << "_" << "Xuc" << uc_in_x << "_" << "Yuc" << uc_in_y << "_" << "FreeSp" << free_space << ".dat";
-}
-//IF YOU WANT TO CALCULATE UNIT CELL
-vector<double> unit_cell_params;
-
-void calculate_unit_cell_params()
-{
-  //molecules count
-  unit_cell_params.push_back(2);
-  //size along x axis for unit cell
-  unit_cell_params.push_back(11.735);
-  //size along y axis for unit cell
-   unit_cell_params.push_back(9.647);
-
-  //unit cell graph (graph distance, graph angle, molecule angle)
-  //first molecule
-  unit_cell_params.push_back(-0.000888541);
-  unit_cell_params.push_back(90.137772);
-  unit_cell_params.push_back(90.0);
-  //second molecule
-  unit_cell_params.push_back(7.303);
-  unit_cell_params.push_back(143.002);
-  unit_cell_params.push_back(90.253273);
-}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////// GLOBAL VARIABLES ////////////////////////////////////////////////////////////////////////////////////////////
